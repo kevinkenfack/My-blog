92b2349c798e21aa2f82afc76d3f97036dba5c2e
kevinkenfack <kenfackjoelkevin@yahoo.com>
2024-05-11 09:30:29 +0100

    update blog UI

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,14 @@ export default function Footer(): JSX.Element {
   return (
     <footer className="mx-auto mt-8 flex w-full max-w-3xl items-center justify-between px-4 py-4 text-xs md:text-sm lg:px-0 lg:text-base">
       <a
-        href="https://www.cosmicjs.com"
+        href="https://kevinkenfack.com"
         target="_blank"
         className="no-underline"
       >
         <div className="flex items-center space-x-2">
           <CosmicLogo />
           <span className="text-zinc-700 dark:text-zinc-300">
-            Proudly powered by Kenfack
+            Powered by Kevin Kenfack
           </span>
         </div>
       </a>
